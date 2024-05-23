bc675d3bb2ee2877833a5f8b5c50508993b7a2b9
Michal Dobaczewski <michal.dobaczewski@futurice.com>
2024-05-23 10:32:49 +0300

    Provide a mode to run tests against external DT API server for faster iteration

--- internal/testutils/test_dependency_track.go
+++ internal/testutils/test_dependency_track.go
@@ -3,6 +3,7 @@ package testutils
 import (
 	"context"
 	"fmt"
+	"os"
 
 	dtrack "github.com/futurice/dependency-track-client-go"
 	"github.com/google/uuid"
@@ -10,9 +11,14 @@ import (
 	"github.com/testcontainers/testcontainers-go/wait"
 )
 
+const ExternalDependencyTrackEndpointEnvVarName = "TF_ACC_EXTERNAL_DEPENDENCY_TRACK_ENDPOINT"
+const ExternalDependencyTrackApikeyEnvVarName = "TF_ACC_EXTERNAL_DEPENDENCY_TRACK_APIKEY"
+
 const defaultDependencyTrackUser = "admin"
 const defaultDependencyTrackPassword = "admin"
 
+// TestDependencyTrack represents the Dependency-Track instance against which the tests will
+// be run, and an API client to access it.
 type TestDependencyTrack struct {
 	Endpoint string
 	ApiKey   string
@@ -21,7 +27,135 @@ type TestDependencyTrack struct {
 	container testcontainers.Container
 }
 
+// InitTestDependencyTrack is a utility function intended to be used within TestMain to
+// initialize a TestDependencyTrack. Returns a TestDependencyTrack and a function to
+// clean-up after the test.
+func InitTestDependencyTrack() (testDependencyTrack *TestDependencyTrack, cleanup func()) {
+	var err error
+	testDependencyTrack, err = NewTestDependencyTrack()
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Failed to initialize test: %v\n", err)
+		os.Exit(1)
+	}
+
+	cleanup = func() {
+		if err := testDependencyTrack.Close(); err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Failed to clean-up test: %v\n", err)
+			// not calling os.Exit(1) from here to let the test report its results
+		}
+	}
+
+	return
+}
+
+// NewTestDependencyTrack returns a TestDependencyTrack. Close it with Close() after the test.
+//
+// Supported environment variables:
+//
+//   - TF_ACC_EXTERNAL_DEPENDENCY_TRACK_ENDPOINT - an HTTP(S) URL to the Dependency-Track API server you
+//     wish to use for testing. WARNING: the tests will affect its state at least while they run. Only use
+//     a dedicated test instance for this.
+//
+//   - TF_ACC_EXTERNAL_DEPENDENCY_TRACK_APIKEY - the API key to the Dependency-Track API server defined by
+//     TF_ACC_EXTERNAL_DEPENDENCY_TRACK_ENDPOINT. The API key should have all permissions. This variable must
+//     be defined if TF_ACC_EXTERNAL_DEPENDENCY_TRACK_ENDPOINT is defined, and must not be defined otherwise.
+//
+// If TF_ACC_EXTERNAL_DEPENDENCY_TRACK_ENDPOINT is empty/unset an internal Dockerized Dependency-Track will be
+// started and configured for the test, to be discarded after the test.
 func NewTestDependencyTrack() (*TestDependencyTrack, error) {
+	config, err := getTestDependencyTrackConfig()
+	if err != nil {
+		return nil, fmt.Errorf("invalid test Dependency-Track configuration: %w", err)
+	}
+
+	if config.isExternalDependencyTrackConfigured() {
+		return newTestDependencyTrackFromExternalEndpoint(config)
+	} else {
+		return newTestDependencyTrackFromInternalContainer()
+	}
+}
+
+// AddProviderConfiguration prepends a provider configuration to a piece of Terraform code (HCL) defining resources.
+func (tdt *TestDependencyTrack) AddProviderConfiguration(terraformCode string) string {
+	return fmt.Sprintf(`
+provider "dependencytrack" {
+  host    = %[1]q
+  api_key = %[2]q
+}
+
+%s
+`, tdt.Endpoint, tdt.ApiKey, terraformCode)
+}
+
+// Close disposes of the test Dependency-Track API server. If an internal Docker container was started, it will be discarded.
+// In case an external API server was used it will not be affected by Close.
+func (tdt *TestDependencyTrack) Close() error {
+	if tdt.container != nil {
+		err := stopDependencyTrackContainer(context.Background(), tdt.container)
+		if err != nil {
+			return err
+		}
+	}
+
+	fmt.Printf("Test Dependency-Track closed")
+
+	return nil
+}
+
+type testDependencyTrackConfig struct {
+	endpoint string
+	apiKey   string
+}
+
+func (c *testDependencyTrackConfig) validate() error {
+	if (c.endpoint == "") != (c.apiKey == "") {
+		return fmt.Errorf(
+			"both %s and %s environment variables must be defined if either of them is",
+			ExternalDependencyTrackEndpointEnvVarName,
+			ExternalDependencyTrackApikeyEnvVarName,
+		)
+	}
+
+	return nil
+}
+
+func (c *testDependencyTrackConfig) isExternalDependencyTrackConfigured() bool {
+	return (c.endpoint != "") && (c.apiKey != "")
+}
+
+func getTestDependencyTrackConfig() (*testDependencyTrackConfig, error) {
+	config := &testDependencyTrackConfig{
+		endpoint: os.Getenv(ExternalDependencyTrackEndpointEnvVarName),
+		apiKey:   os.Getenv(ExternalDependencyTrackApikeyEnvVarName),
+	}
+
+	if err := config.validate(); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func newTestDependencyTrackFromExternalEndpoint(config *testDependencyTrackConfig) (*TestDependencyTrack, error) {
+	client, err := dtrack.NewClient(
+		config.endpoint,
+		dtrack.WithAPIKey(config.apiKey),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("could not create Dependency-Track client: %w", err)
+	}
+
+	fmt.Printf("Using extrernal Dependency-Track with endpoint: %s\n", config.endpoint)
+
+	return &TestDependencyTrack{
+		Endpoint:  config.endpoint,
+		ApiKey:    config.apiKey,
+		Client:    client,
+		container: nil,
+	}, nil
+}
+
+func newTestDependencyTrackFromInternalContainer() (*TestDependencyTrack, error) {
 	ctx := context.Background()
 
 	container, err := startDependencyTrackContainer(ctx)
@@ -41,31 +175,9 @@ func NewTestDependencyTrack() (*TestDependencyTrack, error) {
 		return nil, fmt.Errorf("could not configure Dependency-Track container: %w", err)
 	}
 
-	fmt.Printf("Test Dependency-Track is ready\n")
+	fmt.Printf("Inernal test Dependency-Track is ready\n")
 
 	return testDependencyTrack, nil
-}
-
-func (tdt *TestDependencyTrack) AddProviderConfiguration(terraformCode string) string {
-	return fmt.Sprintf(`
-provider "dependencytrack" {
-  host    = %[1]q
-  api_key = %[2]q
-}
-
-%s
-`, tdt.Endpoint, tdt.ApiKey, terraformCode)
-}
-
-func (tdt *TestDependencyTrack) Close() error {
-	err := stopDependencyTrackContainer(context.Background(), tdt.container)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("Test Dependency-Track closed")
-
-	return nil
 }
 
 func startDependencyTrackContainer(ctx context.Context) (testcontainers.Container, error) {
@@ -97,7 +209,7 @@ func configureDependencyTrackContainer(ctx context.Context, container testcontai
 		return nil, fmt.Errorf("could not get Dependency-Track container endpoint: %w", err)
 	}
 
-	fmt.Printf("Test Dependency-Track endpoint is: %s\n", containerEndpoint)
+	fmt.Printf("Test Dependency-Track container endpoint is: %s\n", containerEndpoint)
 
 	token, err := loginAsDefaultUser(ctx, containerEndpoint)
 	if err != nil {
@@ -165,8 +277,8 @@ func createAllPowerfulApiKey(ctx context.Context, client *dtrack.Client) (apiKey
 
 	fmt.Printf("Created team %s\n", teamName)
 
-	// FIXME try to improve this magic number
-	allPermissions, err := client.Permission.GetAll(ctx, dtrack.PageOptions{PageSize: 1000})
+	// all signs point to paging options being ignored by this endpoint
+	allPermissions, err := client.Permission.GetAll(ctx, dtrack.PageOptions{})
 	if err != nil {
 		return "", fmt.Errorf("could not get Dependency-Track permissions: %w", err)
 	}
