08da04012cbd4b46c07b38ea4d9cbeb81fe02e5d
Michal Dobaczewski <michal.dobaczewski@futurice.com>
2024-07-24 12:30:01 +0300

    Remove unused function TFStringSetToStringSlice

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -1,11 +1,8 @@
 package utils_test
 
 import (
-	"context"
 	"github.com/futurice/terraform-provider-dependencytrack/internal/utils"
 	"github.com/google/uuid"
-	"github.com/hashicorp/terraform-plugin-framework/types"
-	"reflect"
 	"testing"
 )
 
@@ -27,71 +24,5 @@ func TestParseUUID_invalid(t *testing.T) {
 
 	if !diags.HasError() {
 		t.Errorf("Error expected, but received none")
-	}
-}
-
-func TestTFStringSetToStringSlice_basic(t *testing.T) {
-	ctx := context.Background()
-
-	testStrings := []string{"a", "b"}
-	testSet, _ := types.SetValueFrom(ctx, types.StringType, testStrings)
-
-	result, diags := utils.TFStringSetToStringSlice(ctx, testSet)
-
-	if !reflect.DeepEqual(result, testStrings) {
-		t.Errorf("Parsed strings [%v] are different than expected [%v]", result, testStrings)
-	}
-
-	if diags.HasError() {
-		t.Errorf("Unexpected error: %v", diags)
-	}
-}
-
-func TestTFStringSetToStringSlice_empty(t *testing.T) {
-	ctx := context.Background()
-
-	var testStrings []string
-	testSet, _ := types.SetValueFrom(ctx, types.StringType, testStrings)
-
-	result, diags := utils.TFStringSetToStringSlice(ctx, testSet)
-
-	if !reflect.DeepEqual(result, testStrings) {
-		t.Errorf("Parsed strings [%v] are different than expected [%v]", result, testStrings)
-	}
-
-	if diags.HasError() {
-		t.Errorf("Unexpected error: %v", diags)
-	}
-}
-
-func TestTFStringSetToStringSlice_unknown(t *testing.T) {
-	ctx := context.Background()
-
-	testSet := types.SetUnknown(types.StringType)
-
-	result, diags := utils.TFStringSetToStringSlice(ctx, testSet)
-
-	if result != nil {
-		t.Errorf("Expected nil, got strings [%v]", result)
-	}
-
-	if diags.HasError() {
-		t.Errorf("Unexpected error: %v", diags)
-	}
-}
-
-func TestTFStringSetToStringSlice_null(t *testing.T) {
-	ctx := context.Background()
-
-	testSet := types.SetNull(types.StringType)
-
-	result, diags := utils.TFStringSetToStringSlice(ctx, testSet)
-
-	if result != nil {
-		t.Errorf("Expected nil, got strings [%v]", result)
-	}
-
-	if diags.HasError() {
-		t.Errorf("Unexpected error: %v", diags)
 	}
 }
