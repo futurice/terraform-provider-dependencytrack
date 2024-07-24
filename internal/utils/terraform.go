44295f54e8e5ef648868b94f7bf5bc1ce899352b
Michal Dobaczewski <michal.dobaczewski@futurice.com>
2024-07-24 12:30:01 +0300

    Remove unused function TFStringSetToStringSlice

--- internal/utils/terraform.go
+++ internal/utils/terraform.go
@@ -1,12 +1,9 @@
 package utils
 
 import (
-	"context"
 	"fmt"
 	"github.com/google/uuid"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
-	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 )
 
 // ParseUUID parses the UUID value returning a possible error as Diagnostics instead of error.
@@ -20,30 +17,4 @@ func ParseUUID(uuidString string) (uuid.UUID, diag.Diagnostics) {
 	}
 
 	return id, diags
-}
-
-// TFStringSetToStringSlice extracts strings from a string SetValue.
-func TFStringSetToStringSlice(ctx context.Context, tfSet basetypes.SetValue) ([]string, diag.Diagnostics) {
-	var diags diag.Diagnostics
-
-	if tfSet.IsUnknown() {
-		return nil, diags
-	}
-
-	if tfSet.IsNull() {
-		return nil, diags
-	}
-
-	tfStrings := make([]types.String, 0, len(tfSet.Elements()))
-	diags = tfSet.ElementsAs(ctx, &tfStrings, false)
-	if diags.HasError() {
-		return nil, diags
-	}
-
-	strings := make([]string, len(tfStrings))
-	for i, tfString := range tfStrings {
-		strings[i] = tfString.ValueString()
-	}
-
-	return strings, diags
 }
